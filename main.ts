23a407e3928a4696dcb3e3e8af32801969c8cd12
Egyed József <jegyed50@gmail.com>
2023-07-26 17:42:21 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,30 @@
-let szorzo = 100
-let kozepesfenyesseg = 20
+let szorzo = 10
+let kozepesfenyesseg = 0
 let sotet = 0
+let i = 100
 basic.forever(function () {
     pins.analogWritePin(AnalogPin.P0, kozepesfenyesseg)
     pins.analogWritePin(AnalogPin.P1, kozepesfenyesseg)
     pins.analogWritePin(AnalogPin.P2, kozepesfenyesseg)
-    basic.pause(500)
-    for (let index = 0; index <= 19; index++) {
-        pins.analogSetPeriod(AnalogPin.P0, index * szorzo)
-        basic.pause(100)
-    }
-    for (let index = 0; index <= 19; index++) {
-        pins.analogSetPeriod(AnalogPin.P1, index * szorzo)
-        basic.pause(100)
-    }
-    for (let index = 0; index <= 19; index++) {
-        pins.analogSetPeriod(AnalogPin.P2, index * szorzo)
-        basic.pause(100)
+    basic.showString("P")
+    basic.pause(200)
+    for (let index = 0; index <= i; index++) {
+        pins.analogWritePin(AnalogPin.P0, index * szorzo)
+        basic.pause(50)
     }
     pins.analogWritePin(AnalogPin.P0, sotet)
-    pins.analogWritePin(AnalogPin.P1, sotet)
+    basic.showString("Z")
+    basic.pause(200)
+    for (let index = 0; index <= i; index++) {
+        pins.analogWritePin(AnalogPin.P2, index * szorzo)
+        basic.pause(50)
+    }
     pins.analogWritePin(AnalogPin.P2, sotet)
+    basic.showString("K")
+    basic.pause(200)
+    for (let index = 0; index <= i; index++) {
+        pins.analogWritePin(AnalogPin.P1, index * szorzo)
+        basic.pause(50)
+    }
+    pins.analogWritePin(AnalogPin.P1, sotet)
 })
